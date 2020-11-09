db9ccce6f760d86efd518471a48d97d5b62387c8
Razi <razi@journeyo.com>
2020-11-09 18:39:13 +0200

    [WIP] Added routing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Razi's Home Task v1.0
 * This project is using React & Redux /w redux-thunk to manage async calls through redux
+* This project uses react-router-dom (v6.13.4) to handle client-side navigation
 * This project is structured based on the MVC approach, seperating each page to page(model) / view / controller
 * Linting: ESlint abnb with some minor personal adjustments
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+// Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Universal
+import { HOME, FAVORITES } from './universal/pages';
 import './App.scss';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Router>
+      <Switch>
+        <Route path="/" exact render={(props) => (<HomeIndex {...props} page={HOME} />)} />
+        <Route path={`/${FAVORITES}`} exact render={(props) => (<FavoritesIndex {...props} page={FAVORITES} />)} />
+      </Switch>
+    </Router>
+  </>
+);
 
 export default App;
