e9abbe740d0720abe0143910fa873cb92237cc82
Razi <razi@journeyo.com>
2020-11-09 18:27:24 +0200

    [INIT] Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// Material UI
+import Button from '@material-ui/core/Button';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="secondary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
